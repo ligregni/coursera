ab31f11369938f2c25062bc3eb984da504171257
Sergio Ligregni <ligregni@ligI7.letskode.org>
2015-03-30 22:38:46 -0700

    Minor refactoring 'encapsulating' some functions

--- designalgo2/lib/library.py
+++ designalgo2/lib/library.py
@@ -16,13 +16,6 @@ class MinHeap:
         self.heap = list()
         self.size = 0
 
-    def swap(self, origin, dest):
-        tmp = self.heap[origin]
-        self.heap[origin] = self.heap[dest]
-        self.heap[dest] = tmp
-        self.mapa[self.heap[origin][1]] = origin
-        self.mapa[self.heap[dest][1]] = dest
-
     def insert(self, key, item):
         if self.size >= len(self.heap):
             self.heap.append((key, item))
@@ -30,39 +23,19 @@ class MinHeap:
             self.heap[self.size] = (key, item)
         self.mapa[item] = self.size
         self.size += 1
-        self.raiseitem(self.size-1)
+        self.__raiseitem__(self.size-1)
 
-    def raiseitem(self, index):
-        while index != 0:
-            if self.heap[PARENT(index)][0] > self.heap[index][0]:
-                self.swap(PARENT(index), index)
-            index = PARENT(index)
-
-    def sink(self, index):
-        sunk = True
-        while sunk:
-            sunk = False
-            tmp = index
-            if LEFT(index) < self.size and self.heap[LEFT(index)][0] < self.heap[tmp][0]:
-                tmp = LEFT(index)
-            if RIGHT(index) < self.size and self.heap[RIGHT(index)][0] < self.heap[tmp][0]:
-                tmp = RIGHT(index)
-            if tmp != index:
-                self.swap(tmp, index)
-                index = tmp
-                sunk = True
+    def delete(self, item):
+        index = self.mapa[item]
+        self.__swap__(self.size-1, index)
+        self.size -= 1
+        self.__sink__(index)
+        del self.mapa[item]
 
     def increase(self, item, key):
         if self.heap[self.mapa[item]][0] > key:
             self.heap[self.mapa[item]] = (key, self.heap[self.mapa[item]][1])
-            self.raiseitem(self.mapa[item])
-
-    def delete(self, item):
-        index = self.mapa[item]
-        self.swap(self.size-1, index)
-        self.size -= 1
-        self.sink(index)
-        del self.mapa[item]
+            self.__raiseitem__(self.mapa[item])
 
     def size(self):
         return self.size
@@ -72,3 +45,30 @@ class MinHeap:
 
     def top(self):
         return self.heap[0]
+
+    def __swap__(self, origin, dest):
+        tmp = self.heap[origin]
+        self.heap[origin] = self.heap[dest]
+        self.heap[dest] = tmp
+        self.mapa[self.heap[origin][1]] = origin
+        self.mapa[self.heap[dest][1]] = dest
+
+    def __raiseitem__(self, index):
+        while index != 0:
+            if self.heap[PARENT(index)][0] > self.heap[index][0]:
+                self.__swap__(PARENT(index), index)
+            index = PARENT(index)
+
+    def __sink__(self, index):
+        sunk = True
+        while sunk:
+            sunk = False
+            tmp = index
+            if LEFT(index) < self.size and self.heap[LEFT(index)][0] < self.heap[tmp][0]:
+                tmp = LEFT(index)
+            if RIGHT(index) < self.size and self.heap[RIGHT(index)][0] < self.heap[tmp][0]:
+                tmp = RIGHT(index)
+            if tmp != index:
+                self.__swap__(tmp, index)
+                index = tmp
+                sunk = True
