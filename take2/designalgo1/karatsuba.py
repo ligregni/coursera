b2bfcd545ea201d3caa35c04f0fb868c942bcc1f
Sergio Ligregni <sergio@letskode.org>
2020-06-01 22:58:39 +0200

    Implement addition.

--- take2/designalgo1/karatsuba.py
+++ take2/designalgo1/karatsuba.py
@@ -12,10 +12,15 @@ def add(A: str, B: str) -> str:
   result = ''
 
   carry = 0
-  for a, b in itertools.zip_longest(A, B):
+  for a, b in itertools.zip_longest(A[::-1], B[::-1]):
     x = ord(a) - ord('0') if a else 0
     x += ord(b) - ord('0') if b else 0
-    result += chr(x % 10)
+    x += carry
+    result += chr((x % 10) + ord('0'))
     carry = x // 10
 
-  return '0'
+  result += '1' if carry else ''
+
+  return result[::-1]
+
+
--- take2/designalgo1/karatsuba_test.py
+++ take2/designalgo1/karatsuba_test.py
@@ -8,3 +8,28 @@ class AdditionTest(unittest.TestCase):
 
   def test_zero_when_two_zeroes(self):
     self.assertEqual(karatsuba.add('0', '0'), '0')
+
+  def test_single_digit_a_when_b_is_zero(self):
+    self.assertEqual(karatsuba.add('9', '0'), '9')
+
+  def test_single_digit_b_when_a_is_zero(self):
+    self.assertEqual(karatsuba.add('0', '9'), '9')
+
+  def test_a_when_b_is_zero(self):
+    self.assertEqual(karatsuba.add('9876200', '0'), '9876200')
+
+  def test_b_when_a_is_zero(self):
+    self.assertEqual(karatsuba.add('0', '9876200'), '9876200')
+
+  def test_a_plus_b_no_carry_needed(self):
+    self.assertEqual(karatsuba.add('1111', '2222'), '3333')
+
+  def test_a_plus_b_carry_needed(self):
+    self.assertEqual(karatsuba.add('1', '9999'), '10000')
+
+  def test_a_plus_b_large_b_larger_a(self):
+    self.assertEqual(karatsuba.add('123456', '9999'), '133455')
+
+  def test_a_plus_b_large_a_larger_b(self):
+    self.assertEqual(karatsuba.add('9999', '123456'), '133455')
+
