71bbb5b5c351d096a35cc0b388d2a0a6b7db077d
Sergio Ligregni <sergio@letskode.org>
2020-06-02 00:42:25 +0200

    Full implementation of Karatsuba's multiplication.

--- take2/designalgo1/karatsuba_test.py
+++ take2/designalgo1/karatsuba_test.py
@@ -33,3 +33,69 @@ class AdditionTest(unittest.TestCase):
   def test_a_plus_b_large_a_larger_b(self):
     self.assertEqual(karatsuba.add('9999', '123456'), '133455')
 
+
+class SubtractionTest(unittest.TestCase):
+
+  def test_zero_when_two_zeroes(self):
+    self.assertEqual(karatsuba.subtract('0', '0'), '0')
+
+  def test_zero_when_equal(self):
+    self.assertEqual(karatsuba.subtract('1234', '1234'), '0')
+
+  def test_minuendo_when_substraendo_is_zero(self):
+    self.assertEqual(karatsuba.subtract('1234', '0'), '1234')
+
+  def test_subtraction_minus_one(self):
+    self.assertEqual(karatsuba.subtract('1234', '1'), '1233')
+
+  def test_subtraction_minus_all_but_one(self):
+    self.assertEqual(karatsuba.subtract('1234', '1233'), '1')
+
+  def test_subtraction_no_borrow(self):
+    self.assertEqual(karatsuba.subtract('9876', '4321'), '5555')
+
+  def test_subtraction_borrow_follows(self):
+    self.assertEqual(karatsuba.subtract('10000', '1'), '9999')
+
+  def test_subtraction_borrow(self):
+    self.assertEqual(karatsuba.subtract('10000', '1234'), '8766')
+
+
+class GetHalvesTest(unittest.TestCase):
+
+  def test_halves_large_number_even_digits(self):
+    self.assertEqual(karatsuba.get_halves('12345678', 4), ('1234', '5678'))
+
+  def test_halves_large_number_odd_digits(self):
+    self.assertEqual(karatsuba.get_halves('1234567', 4), ('123', '4567'))
+
+  def test_halves_small_number_one_and_one(self):
+    self.assertEqual(karatsuba.get_halves('12', 1), ('1', '2'))
+
+  def test_halves_small_number_large_n(self):
+    self.assertEqual(karatsuba.get_halves('12', 2), ('0', '12'))
+
+  def test_halves_small_number_largest_n(self):
+    self.assertEqual(karatsuba.get_halves('12', 3), ('0', '12'))
+
+
+class MultiplicationTest(unittest.TestCase):
+
+  def test_multiply_by_zero_a(self):
+    self.assertEqual(karatsuba.multiply('1234', '0'), '0')
+
+  def test_multiply_by_zero_b(self):
+    self.assertEqual(karatsuba.multiply('0', '1234'), '0')
+
+  def test_multiply_by_one_a(self):
+    self.assertEqual(karatsuba.multiply('1234', '1'), '1234')
+
+  def test_multiply_by_one_b(self):
+    self.assertEqual(karatsuba.multiply('1', '1234'), '1234')
+
+  def test_multiply_one_digit(self):
+    self.assertEqual(karatsuba.multiply('2', '7'), '14')
+
+  def test_multiply_many_digits(self):
+    self.assertEqual(karatsuba.multiply('1234', '5678'), '7006652')
+
